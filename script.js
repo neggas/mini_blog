6c2a602c1514558d1f6028fcb738c7d87840ff8e
neggas <arsene.fop@gmail.com>
2020-04-19 00:29:06 +0200

    rendu dynamique cote reponse

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
-const { log } = console;
-const app = document.getElementById("msg");
+const app = document.getElementById("articles");
+const img = document.getElementById("image");
+
+let articles = [{
+    id: 1,
+    titre: "lorem 1",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, animi doloremque! Maxime sunt aliquid animi distinctio cupiditate impedit, vel quos, quam molestias quibusdam, mollitia ab dolorum voluptatem quia numquam amet.",
+    article: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, animi doloremque! Maxime sunt aliquid animi distinctio cupiditate impedit, vel quos, quam molestias quibusdam, mollitia ab dolorum voluptatem quia numquam amet.",
+    img: "img/cartoon-doctor-5022797_640.jpg"
+}, {
+    id: 2,
+    titre: "lorem 2",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, animi doloremque! Maxime sunt aliquid animi distinctio cupiditate impedit, vel quos, quam molestias quibusdam, mollitia ab dolorum voluptatem quia numquam amet.",
+    article: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, animi doloremque! Maxime sunt aliquid animi distinctio cupiditate impedit, vel quos, quam molestias quibusdam, mollitia ab dolorum voluptatem quia numquam amet.",
+    img: "img/javascript-4523100_640.jpg"
+}]
 
 
 window.onload = () => {
@@ -9,15 +23,29 @@ window.onload = () => {
 }
 
 function output(id) {
-    log(typeof id);
+    let article = articles[id - 1];
+    let view = `
+    <div class="txt-zonne">
+        <h3>${article.titre}</h3>
+        <p>
+           ${article.article}
+        </p>
+    </div>
+
+    <div id="commentaire">
+        <textarea name="" id="" cols="30" rows="10">
+
+        </textarea>
+        <button>Repondre</button>
+    </div>
+`
+
+    img.src = article.img;
+    app.innerHTML = view;
 }
 
 function extractId(search) {
     let param = search.substring(search.indexOf('?') + 1, search.length);
-
-
     param = parseInt(param.split("=")[1]);
-
     return param;
-
 }
